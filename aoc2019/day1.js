61ed8ea5a83b6ba450308dfe8bc1620097026fca
Christian Leskowsky <christian@leskowsky.net>
2020-07-13 17:48:48 -0400

    Added aoc2019 day 1 part 2

--- aoc2019/day1.js
+++ aoc2019/day1.js
@@ -1,8 +1,40 @@
+// Part 1
+
 // Returns fuel required to lift a list of integer masses
-function needsFuel(m) {
-    return m
-        .map((x) => Math.floor(x / 3) - 2)
-        .reduce((sum, x) => sum += x, 0);
+function massesNeedFuel(masses) {
+    return masses
+        .map((m) => Math.floor(m / 3) - 2)
+        .reduce((sum, m) => sum += m, 0);
 }
 
-module.exports = needsFuel;
+module.exports = {
+    // Part 1
+    massesNeedFuel: massesNeedFuel,
+
+    // Part 2
+    fuelHasWeightToo: fuelHasWeightToo
+}
+
+// Part 2
+
+function getFuelForMass(m) {
+    return Math.floor(m / 3) - 2;
+}
+
+function fuelHasWeightToo(masses) {
+    let sum = 0;
+    masses.forEach((m) => {
+        let f = getFuelForMass(m);
+        sum += f;
+
+        let x = getFuelForMass(f);
+        while (x > 0) {
+            sum += x;
+
+            // Calculate fuel needed for f
+            f = x;
+            x = getFuelForMass(f);
+        }
+    });
+    return sum;
+}
--- aoc2019/day1.test.js
+++ aoc2019/day1.test.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
 
-const needsFuel = require('./day1');
+const { massesNeedFuel, fuelHasWeightToo } = require('./day1');
 
 test('Fuel to lift mass: 100756 is 33583', () => {
-    expect(needsFuel([100756])).toBe(33583);
+    expect(massesNeedFuel([100756])).toBe(33583);
 });
 
-test('With puzzle input', () => {
-    const input = fs.readFileSync('input/1.txt', "utf-8")
+function input() {
+    return fs.readFileSync('input/1.txt', "utf-8")
         .split('\n')
         .map((x) => parseInt(x))
         .filter((x) => !isNaN(x));
-    expect(needsFuel(input)).toBe(3390596);
+}
+
+test('With puzzle input', () => {
+    expect(massesNeedFuel(input())).toBe(3390596);
+});
+
+test('Test fuel has weight too', () => {
+    expect(fuelHasWeightToo([1969])).toBe(966);
+    expect(fuelHasWeightToo([100756])).toBe(50346);
+    expect(fuelHasWeightToo(input())).toBe(5083024);
 });
