d811e9853d820fe9d58aa0392bcc44c31020bae7
Christian Leskowsky <christian@leskowsky.net>
2020-12-09 20:22:55 -0500

    Day 9 parts a,b

--- aoc2020/src/main/java/a/Day9.java
+++ aoc2020/src/main/java/a/Day9.java
@@ -2,46 +2,56 @@ package a;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.List;
 
 public class Day9 {
     public static void main(String[] args) throws Exception {
-        System.out.println("hello");
-
         List<String> input = Files.readAllLines(Path.of("inputs/9.txt"));
         long[] ciphertext = new long[input.size()];
         for (int i = 0; i < input.size(); i++) {
             ciphertext[i] = Long.parseLong(input.get(i));
         }
-        System.out.println(ciphertext);
 
         // Part a
-        for (int i = 5; i < ciphertext.length; i++) {
+        for (int i = 25; i < ciphertext.length; i++) {
             long next = ciphertext[i];
-            int preambleStart = i - 5;
-            int preambleEnd = i;
-            for (int j = preambleStart; j < preambleEnd; j++) {
-                long x = ciphertext[j];
-                for (int k = preambleStart; k < preambleEnd; k++) {
-                    long y = ciphertext[k];
-                    if (x == y) {
-                        continue;
-                    }
+            if (!isValidNext(ciphertext, i-25, i-1, next)) {
+                System.out.println(next);
+                break;
+            }
+        }
 
+        // Part b
+        long myNumber = 32321523;
+        for (int i = 0; i < ciphertext.length; i++) {
+            long sum = 0;
+            for (int j = i; j < ciphertext.length; j++) {
+                sum += ciphertext[j];
+                if (sum > myNumber) {
+                    break;
+                }
+                if (sum == myNumber) {
+                    System.out.printf("Found! i: %d, j: %d, ct[i]: %d, ct[j]: %d%n",
+                            i, j, ciphertext[i], ciphertext[j]);
+
+                    // smallest: 1145730
+                    // biggest: 3649251
+                    System.out.println(1145730 + 3649251);
+
+                    System.exit(0);
                 }
             }
         }
     }
 
-    public static boolean isValidNext(long[] preamble, long l) {
-        for (int j = 1; j <= 5; j++) {
-            long x = preamble[i - j];
-            for (int k = 1; k <= 5; k++) {
-                if (j == k) {
+    public static boolean isValidNext(long[] ciphertext, int preambleStart, int preambleEnd, long l) {
+        for (int i = preambleStart; i <= preambleEnd; i++) {
+            long x = ciphertext[i];
+            for (int j = preambleStart; j <= preambleEnd; j++) {
+                if (i == j) {
                     continue;
                 }
-                long y = preamble[k];
+                long y = ciphertext[j];
                 if (x == y) {
                     continue;
                 }
@@ -50,5 +60,6 @@ public class Day9 {
                 }
             }
         }
+        return false;
     }
 }
