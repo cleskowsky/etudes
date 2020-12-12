40599720260e71cecbaf7fa997d65bf65f244a56
Christian Leskowsky <christian@leskowsky.net>
2020-12-12 00:22:49 -0500

    Day 10 part a

--- aoc2020/src/main/java/a/Day10.java
+++ aoc2020/src/main/java/a/Day10.java
@@ -10,15 +10,38 @@ import java.util.stream.Collectors;
 
 public class Day10 {
     public static void main(String[] args) throws Exception {
-        var adapters = Input("inputs/10sample.txt");
+//        var adapters = Input("inputs/10sample.txt");
+        var adapters = Input("inputs/10.txt");
         for (Integer a : adapters) {
             System.out.println(a);
         }
+        // Add my seat's charging outlet
+        adapters.add(0, 0);
 
         // Part a
-        List<Integer> using = new ArrayList<>();
-        List<Integer> next = new ArrayList<>();
-        int curr = 0;
+        int oneJolt = 0;
+        // threeJolts starts at 1 to account for my device
+        int threeJolts = 1;
+        for (int i = 0; i < adapters.size() - 1; i++) {
+            int curr = adapters.get(i);
+            int next = adapters.get(i + 1);
+            switch (next - curr) {
+                case 1:
+                    oneJolt++;
+                    break;
+                case 3:
+                    threeJolts++;
+                    break;
+                default:
+                    System.out.printf("Difference in jolts is not 1 or 3: %d, i: %d%n", next - curr, i);
+                    break;
+            }
+        }
+        System.out.println("oneJolts: " + oneJolt);
+        System.out.println("threeJolts: " + threeJolts);
+        System.out.println("1s * 3s: " + (oneJolt * threeJolts));
+
+        // Part b
 
     }
 
@@ -26,8 +49,8 @@ public class Day10 {
         List<Integer> lines = new ArrayList<>();
         try {
             lines = Files.readAllLines(Path.of(fileName)).stream()
-                .map(s -> Integer.parseInt(s))
-                .collect(Collectors.toList());
+                    .map(s -> Integer.parseInt(s))
+                    .collect(Collectors.toList());
             Collections.sort(lines);
         } catch (IOException e) {
             System.out.println("Couldn't read file: " + fileName);
