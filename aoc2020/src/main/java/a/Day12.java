0b4855c9ca0e86cef8fbe18884ce38fb24d905b4
Christian Leskowsky <christian@leskowsky.net>
2020-12-15 17:38:36 -0500

    Day 12 parta Removed the heading enum since it wasn't really getting me anything

--- aoc2020/src/main/java/a/Day12.java
+++ aoc2020/src/main/java/a/Day12.java
@@ -4,20 +4,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Day12 {
-    enum Heading {
-        NORTH(0, 1),
-        EAST(1, 0),
-        SOUTH(0, -1),
-        WEST(-1, 0);
-
-        private int dx, dy;
-
-        Heading(int dx, int dy) {
-            this.dx = dx;
-            this.dy = dy;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         // Input
 //        String s = Files.readString(Path.of("inputs/12sample.txt"));
@@ -27,62 +13,62 @@ public class Day12 {
         int x = 0;
         int y = 0;
 
-        Heading[] headings = new Heading[]{
-                Heading.NORTH,
-                Heading.EAST,
-                Heading.SOUTH,
-                Heading.WEST
+        int[][] headings = new int[][]{
+                {0, 1},  // North
+                {1, 0},  // East
+                {0, -1}, // South
+                {-1, 0}  // West
         };
-        int facing = 1;
+        int facing = 1;  // East
 
         for (String instr : s.split("\\n")) {
             String move = instr.substring(0, 1);
             int by = Integer.parseInt(instr.substring(1));
 
             // Are we changing our facing direction?
-            if (move.equals("R") || move.equals("L")) {
-                facing = turn(facing, move, by);
+            if (move.equals("L") || move.equals("R")) {
+                facing = turn(facing, move.equals("L"), by);
                 continue;
             }
 
             // Move in the facing direction or ...
-            Heading h;
+            int[] h;
             if (move.equals("F")) {
                 h = headings[facing];
             } else {
-                h = getHeading(move);
+                switch (move) {
+                    case "N":
+                        h = headings[0];
+                        break;
+                    case "E":
+                        h = headings[1];
+                        break;
+                    case "S":
+                        h = headings[2];
+                        break;
+                    case "W":
+                        h = headings[3];
+                        break;
+                    default:
+                        throw new RuntimeException("Invalid move: " + move);
+                }
             }
-            x += h.dx * by;
-            y += h.dy * by;
+            x += h[0] * by;
+            y += h[1] * by;
         }
         System.out.println(Math.abs(x) + Math.abs(y));
 
         // Partb
     }
 
-    public static Heading getHeading(String s) {
-        switch (s) {
-            case "N":
-                return Heading.NORTH;
-            case "E":
-                return Heading.EAST;
-            case "S":
-                return Heading.SOUTH;
-            case "W":
-                return Heading.WEST;
-            default:
-                throw new IllegalArgumentException("Bad heading: " + s);
-        }
-    }
-
-    public static int turn(int facing, String dir, int by) {
-        if (dir.equals("R")) {
-            facing = (facing + by / 90) % 4;
-        } else {
+    public static int turn(int facing, boolean left, int by) {
+        if (left) {
             facing -= by / 90;
             if (facing < 0) {
                 facing = 4 + facing;
             }
+        } else {
+            facing = (facing + by / 90) % 4;
         }
         return facing;
     }
--- aoc2020/src/test/java/a/Day12Test.java
+++ aoc2020/src/test/java/a/Day12Test.java
@@ -7,8 +7,8 @@ class Day12Test {
 
     @Test
     void turn() {
-        Assertions.assertEquals(3, Day12.turn(0, "L", 90));
-        Assertions.assertEquals(2, Day12.turn(0, "L", 180));
-        Assertions.assertEquals(2, Day12.turn(0, "R", 180));
+        Assertions.assertEquals(3, Day12.turn(0, true, 90));
+        Assertions.assertEquals(2, Day12.turn(0, true, 180));
+        Assertions.assertEquals(2, Day12.turn(0, false, 180));
     }
 }
