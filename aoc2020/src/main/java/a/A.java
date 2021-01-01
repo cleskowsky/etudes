400350a0b5c7c4f95499ad5c0e26408fb6f12680
Christian Leskowsky <christian@leskowsky.net>
2021-01-01 10:18:10 -0500

    Day 14 partb sol. Did the second part using strings not bit ops which is possibly skipping the point. Maybe I'll come back and try with bitops later. Performance isn't really an issue here because the input is so small
    Memory usage and string work would be horrendous here for larger inputs

--- aoc2020/src/main/java/a/A.java
+++ aoc2020/src/main/java/a/A.java
@@ -4,18 +4,6 @@ import java.util.Arrays;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
-        Integer[] arr = new Integer[]{3, 1, 2};
-        Arrays.sort(arr, (x, y) -> {
-            if (x < y) {
-                return 1;
-            } else if (x > y) {
-                return -1;
-            } else {
-                return 0;
-            }
-    });
-//        Arrays.sort(arr);
-        System.out.println(arr);
-}
+        System.out.println(Integer.toBinaryString(1 << 31));
+    }
 }
--- aoc2020/src/main/java/a/Day14.java
+++ aoc2020/src/main/java/a/Day14.java
@@ -1,12 +1,17 @@
 package a;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Day14 {
-    public static boolean DEBUG = false;
+    public static boolean debug = false;
+    public static boolean partA = false;
 
     public static void main(String[] args) throws Exception {
         for (String a : args) {
@@ -16,86 +21,104 @@ public class Day14 {
         for (String a : args) {
             switch (a) {
                 case "-v":
-                    DEBUG = true;
+                    debug = true;
+                    break;
+                case "-a":
+                    partA = true;
                     break;
                 default:
                     throw new Exception("Unknown argument: " + a);
             }
         }
 
-//        I haven't done bit shifting in quite a while
-//        Back to the basics! :)
-//        System.out.println(1 << 6 ^ 1 << 1);
-//         66
-
-//        int n = 11;
-//        n = n | (1 << 6);
-//        n = n & ~(1 << 1);
-//        System.out.println(n);
-
-        // and for 0
-        // Xs should be 0
-        // 0 && 1 => 0 good
-        // 0 && 0 => 0 good
-
-        // or for 1
-        // Xs should be 1
-        // 1 && 0 => 0 bad
-        // 1 && 1 => 1 good
-
-        // Our initialization vector includes mask updates and store instructions
-//        var initV = Files.readAllLines(Path.of("inputs/14sample.txt"));
-        var initV = Files.readAllLines(Path.of("inputs/14.txt"));
-        var mem = new HashMap<Integer, Long>();
+        // Input contains SetMask and SetMem instructions
+//        var input = Files.readAllLines(Path.of("inputs/14sample.txt"));
+        var input = Files.readAllLines(Path.of("inputs/14.txt"));
+        var mem = new HashMap<String, Long>();
         long onesMask = 0;
         long zerosMask = Long.MAX_VALUE;
-        for (String s : initV) {
-            if (s.length() == 0) {
-                System.out.println("");
-                continue;
-            }
-            switch (s.substring(0, 4)) {
-                case "mask":
-                    onesMask = 0;
-                    zerosMask = 0;
-                    char[] c = s.split("=")[1].trim().toCharArray();
-                    for (int i = 0; i < c.length; i++) {
-                        switch (c[i]) {
-                            case '1':
-                                onesMask = onesMask | (1l << 35 - i);
-                                break;
-                            case '0':
-                                zerosMask = zerosMask | (1l << 35 - i);
-                                break;
-                            case 'X':
-                                break;
-                            default:
-                                throw new Exception("Couldn't parse bit: " + c[i]);
+        char[] mask = new char[]{};
+        for (String s : input) {
+            switch (s.substring(0, 2)) {
+                case "ma":
+                    if (partA) {
+                        onesMask = 0;
+                        zerosMask = 0;
+                        char[] c = s.split(" = ")[1].toCharArray();
+                        for (int i = 0; i < c.length; i++) {
+                            switch (c[i]) {
+                                case '1':
+                                    onesMask = onesMask | (1l << 35 - i);
+                                    break;
+                                case '0':
+                                    zerosMask = zerosMask | (1l << 35 - i);
+                                    break;
+                                case 'X':
+                                    break;
+                                default:
+                                    throw new Exception("Couldn't parse bit: " + c[i]);
+                            }
+                        }
+                        zerosMask = ~zerosMask;
+                        if (debug) {
+                            System.out.println("Input: " + new String(c));
+                            System.out.println("ZerosMask: " + Long.toBinaryString(zerosMask));
+                            System.out.println("OnesMask: " + Long.toBinaryString(onesMask));
+                        }
+                    } else {
+                        // I think the mask will be a bit simpler in part B. Let's see
+                        mask = s.split(" = ")[1].toCharArray();
+                        if (debug) {
+                            System.out.println("Mask: " + new String(mask));
+                            System.out.println("Mask length: " + mask.length);
                         }
                     }
-                    zerosMask = ~zerosMask;
-                    if (DEBUG) {
-                        System.out.println("Input: " + new String(c));
-                        System.out.println("ZerosMask: " + Long.toBinaryString(zerosMask));
-                        System.out.println("OnesMask: " + Long.toBinaryString(onesMask));
-                    }
                     break;
-                case "mem[":
-                    if (DEBUG) {
-                        System.out.println(s);
+                case "me":
+                    // Parse mem line
+                    if (debug) {
+                        System.out.println("Input line: " + s);
                     }
-                    String[] split = s.split("=");
-                    long val = Long.parseLong(split[1].trim());
+                    String[] split = s.split(" = ");
+                    long val = Long.parseLong(split[1]);
                     split = split[0].split("\\[");
-                    int loc = Integer.parseInt(
-                            split[1].substring(0, split[1].indexOf("]"))
-                    );
-                    val &= zerosMask;
-                    val |= onesMask;
-                    if (DEBUG) {
-                        System.out.printf("%d %d%n", loc, val);
+                    String addr = split[1].substring(0, split[1].indexOf("]"));
+
+                    // Puzzles
+                    if (partA) {
+                        val &= zerosMask;
+                        val |= onesMask;
+                        if (debug) {
+                            System.out.printf("Set addr: %s to value: %d%n", addr, val);
+                        }
+                        mem.put(addr, val);
+                    } else {
+                        // Applying mask will give address with floated bits this time
+                        // Further processing needed to get real addresses
+                        addr = Integer.toBinaryString(Integer.parseInt(addr));
+                        // From apache commons. Nice :)
+                        addr = StringUtils.leftPad(addr, 36, "0");
+                        char[] addrMasked = new char[36];
+
+                        char[] c = addr.toCharArray();
+                        for (int i = 0; i < c.length; i++) {
+                            if (mask[i] == '0') {
+                                addrMasked[i] = c[i];
+                                continue;
+                            }
+                            addrMasked[i] = mask[i];
+                        }
+                        if (debug) {
+                            System.out.println("Masked address: " + new String(addrMasked));
+                        }
+                        var floated = getAddresses(addrMasked);
+                        for (String f : floated) {
+                            if (debug) {
+                                System.out.println("Floated address: " + f);
+                            }
+                            mem.put(f, val);
+                        }
                     }
-                    mem.put(loc, val);
                     break;
                 default:
                     throw new Exception("Couldn't parse instruction: " + s);
@@ -104,7 +127,32 @@ public class Day14 {
         long sum = mem.values().stream()
                 .collect(Collectors.summingLong(Long::longValue));
         System.out.println(sum);
-        System.out.println(Long.MAX_VALUE);
-        // too low: 243965416522 :/
+    }
+
+    public static List<String> getAddresses(char[] c) {
+        var addr = new ArrayList<String>();
+        addr.add("");
+        for (int i = 0; i < c.length; i++) {
+            if ("01X".indexOf(c[i]) == -1) {
+                throw new RuntimeException("Invalid char: " + c[i]);
+            }
+            int n = addr.size();
+            for (int j = 0; j < n; j++) {
+                String s = addr.remove(j);
+                switch (c[i]) {
+                    case '0':
+                    case '1':
+                        addr.add(j, s + c[i]);
+                        break;
+                    case 'X':
+                        addr.add(j, s + "0");
+                        addr.add(s + "1");
+                        break;
+                    default:
+                        throw new RuntimeException("Bad first bit: " + c[0]);
+                }
+            }
+        }
+        return addr;
     }
 }
