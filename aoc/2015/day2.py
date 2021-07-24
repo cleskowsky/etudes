904c632508afe367427cdfab479140c9e2005264
Christian Leskowsky <christian@leskowsky.net>
2021-07-24 13:16:41 -0400

    Day 2, part 2

--- aoc/2015/day2.py
+++ aoc/2015/day2.py
@@ -8,6 +8,8 @@ def input(file_name):
 boxes = input("day2.txt")
 print(boxes[:5])
 
+# Part 1
+
 def wrapping_paper_needed(b):
     sides = [mul(*x) for x in combinations(b, 2)]
     sides.sort()
@@ -15,3 +17,24 @@ def wrapping_paper_needed(b):
     return sum(sides + sides + sides[:1])
 
 print(sum(map(wrapping_paper_needed, boxes)))
+
+# Part 2
+
+ribbon_length_needed = 0
+for b in boxes:
+    sides = list(combinations(b, 2))
+
+    # Calculate ribbon required for each side
+    # Add min ribbon length to running sum
+    perimeters = [2 * x[0] + 2 * x[1] for x in sides]
+    ribbon_length_needed += min(perimeters)
+
+    # Calculate ribbon for box volume and add that to running sum too
+    bow_length = b[0] * b[1] * b[2]
+    ribbon_length_needed += bow_length
+
+    print(f"box {b}, sides {sides}, perimeters {perimeters}, min(perimeters) {min(perimeters)}, bow_length {bow_length}")
+
+
+# Final amount of ribbon
+print(ribbon_length_needed)
