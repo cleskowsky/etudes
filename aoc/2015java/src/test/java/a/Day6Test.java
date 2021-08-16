a9b75e8b07a382457569668255596eb3d7f1571d
Christian Leskowsky <christian@leskowsky.net>
2021-08-15 21:02:18 -0400

    Day 6 part 1,2

---
+++ aoc/2015java/src/test/java/a/Day6Test.java
@@ -0,0 +1,25 @@
+package a;
+
+import org.junit.Test;
+
+import a.Day6.Action;
+import a.Day6.Input;
+import a.Day6.Instruction;
+import a.Day6.Point;
+import static org.junit.Assert.assertEquals;
+
+public class Day6Test {
+
+	@Test
+	public void parseInstruction() {
+
+		assertEquals(
+			new Instruction(Action.TURN_ON, new Point(887, 9), new Point(959, 629)),
+			Input.parseInstruction("turn on 887,9 through 959,629")
+		);
+		assertEquals(
+			new Instruction(Action.TOGGLE, new Point(887, 9), new Point(959, 629)),
+			Input.parseInstruction("toggle 887,9 through 959,629")
+		);
+	}
+}
