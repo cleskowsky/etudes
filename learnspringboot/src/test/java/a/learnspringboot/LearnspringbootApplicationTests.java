239db36089f47c50aec08a1e6ccccdb82e44f122
Christian Leskowsky <christian@leskowsky.net>
2021-09-29 21:18:03 -0400

    Create a spring boot starter project with spring web, thymeleaf and devtools dependencies

---
+++ learnspringboot/src/test/java/a/learnspringboot/LearnspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package a.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
