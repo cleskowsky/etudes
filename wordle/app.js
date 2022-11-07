7e62727b678bcc43604fec663d713d13b78bb21e
Christian Leskowsky <christian@leskowsky.net>
2022-11-07 05:42:30 -0500

    Added wordle clone things

--- wordle/app.js
+++ wordle/app.js
@@ -1,3 +1,25 @@
 document.addEventListener('alpine:init', () => {
     console.log('Alpine init called');
+
+    Alpine.data('game', () => {
+        return {
+            guessesAllowed: 3,
+            wordLength: 3,
+            currentRowIndex: 0,
+            currentTileIndex: 0,
+
+            init() {
+                console.log('Init called');
+                this.board = Array.from({length: this.guessesAllowed}, () => {
+                    return Array.from({length: this.wordLength}, () => 'X');
+                });
+            },
+
+            onKeyPress(key) {
+                if (/^[A-z]$/.test(key)) {
+                    this.board[0][0] = key;
+                }
+            }
+        }
+    })
 });
