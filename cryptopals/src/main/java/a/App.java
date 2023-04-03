a07267c2e6d8d88ff944a26d5d370177d01dbc92
Christian Leskowsky <christian@leskowsky.net>
2023-04-02 21:59:20 -0400

    Challenge 1, set 1

--- cryptopals/src/main/java/a/App.java
+++ cryptopals/src/main/java/a/App.java
@@ -1,13 +1,19 @@
 package a;
 
+import com.google.common.io.BaseEncoding;
+
+import java.util.Base64;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        // Challenge 1
+        // Use google guava base16 (hex) encoder / decoder to get byte[]
+        var b = BaseEncoding.base16().decode("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".toUpperCase());
+        // Java has a built in base64 encoder
+        System.out.println(Base64.getEncoder().encodeToString(b));
     }
 }
