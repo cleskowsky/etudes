00d32d68811243cb2f42d6edb0d2351b293e5b73
Christian Leskowsky <christian@leskowsky.net>
2023-06-16 22:01:09 -0400

    Make util.Heading public

--- adventofcode/2022/src/main/java/util/Heading.java
+++ adventofcode/2022/src/main/java/util/Heading.java
@@ -1,12 +1,12 @@
 package util;
 
-enum Heading {
+public enum Heading {
     N(new Point(0, -1)),
     E(new Point(1, 0)),
     S(new Point(0, 1)),
     W(new Point(-1, 0));
 
-    Point p;
+    public Point p;
 
     Heading(Point p) {
         this.p = p;
