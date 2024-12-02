36e83b97812bea8a919082521fcb45f490cf2102
Christian Leskowsky <christian@leskowsky.net>
2024-12-01 21:48:49 -0500

    bit more tidy up

--- adventofcode/2024/day1.ts
+++ adventofcode/2024/day1.ts
@@ -18,28 +18,29 @@ function parseInput(file: string) {
 // const locations = parseInput("day1_sample.txt");
 const { left, right } = parseInput("day1_in.txt");
 
-function sumDistances(left: number[], right: number[]) {
+// Part 1
+
+function totalDistance(left: number[], right: number[]) {
   let sum = 0;
   for (let i = 0; i < left.length; i++) {
-    const x = left[i];
-    const y = right[i];
-    sum += Math.abs(y - x);
+    sum += Math.abs(left[i] - right[i]);
   }
   return sum;
 }
-console.log(sumDistances(left, right));
+console.log(totalDistance(left, right));
+
+// Part 2
 
 const frequencies = new Map();
 for (const x of right) {
   frequencies.set(x, frequencies.get(x) + 1 || 1);
 }
-console.log(frequencies);
 
-function sumSimilarity(left: number[]) {
+function similarityScore(left: number[]) {
   let sum = 0;
   for (let i = 0; i < left.length; i++) {
     sum += left[i] * frequencies.get(left[i]) || 0;
   }
   return sum;
 }
-console.log(sumSimilarity(left));
+console.log(similarityScore(left));
