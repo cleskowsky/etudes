a46ce27590790068e18382178fe84ce445e1d5cd
Christian Leskowsky <christian@leskowsky.net>
2024-12-05 08:11:47 -0500

    Cleaned up a bit

--- adventofcode/2024py/day2.py
+++ adventofcode/2024py/day2.py
@@ -1,6 +1,3 @@
-from runpy import run_path
-
-
 class Report:
     def __init__(self, levels):
         self.levels = levels
@@ -11,7 +8,9 @@ class Report:
 
 def increasing(r):
     for i in range(len(r.levels) - 1):
-        if r.levels[i] > r.levels[i + 1]:
+        curr = r.levels[i]
+        next = r.levels[i + 1]
+        if curr > next:
             return False
 
     return True
@@ -19,7 +18,9 @@ def increasing(r):
 
 def decreasing(r):
     for i in range(len(r.levels) - 1):
-        if r.levels[i] < r.levels[i + 1]:
+        curr = r.levels[i]
+        next = r.levels[i + 1]
+        if curr < next:
             return False
 
     return True
@@ -32,13 +33,15 @@ def safe_report(r):
             if step < 1 or step > 3:
                 return False
 
+        print('safe report')
+        print(r)
         return True
 
     return False
 
 
-assert not safe_report(Report([1, 2, 7, 8, 9]))
 assert safe_report(Report([1, 3, 6, 7, 9]))
+assert not safe_report(Report([1, 2, 7, 8, 9]))
 
 
 def parse_input(filename):
@@ -53,25 +56,31 @@ def parse_input(filename):
     return records
 
 
+def recover_report(r):
+    r_levels = r.levels
+    for i in range(len(r_levels)):
+        r2 = Report(r_levels.copy())
+        del r2.levels[i]
+        if safe_report(r2):
+            print('recovered report')
+            print('unsafe: ', r)
+            print('safe: ', r2)
+            return True
+
+    return False
+
+
 # x = parse_input('day2_sample.txt')
 x = parse_input('day2.txt')
 print(x)
 
-n = 0
+safe_reports = 0
+recovered_reports = 0
 for r in x:
     if safe_report(r):
-        print('safe report')
-        print(r)
-        n += 1
-    else:
-        print('unsafe report found')
-        print(r)
-        for i in range(len(r.levels)):
-            print(r.levels)
-            r2 = Report(r.levels.copy())
-            del r2.levels[i]
-            if safe_report(r2):
-                n += 1
-                break
+        safe_reports += 1
+    elif recover_report(r):
+        recovered_reports += 1
 
-print(n)
+print('safe reports: ', safe_reports)
+print('recovered reports: ', safe_reports + recovered_reports)
