c8d846eba62be2fa35198bee83a2a669ecb6268b
Christian Leskowsky <christian@leskowsky.net>
2024-12-08 07:27:38 -0500

    updated method name

--- adventofcode/2024py/day3.py
+++ adventofcode/2024py/day3.py
@@ -6,14 +6,14 @@ def mult(s):
     return lhs * rhs
 
 
-def run_it(prog):
+def compute(prog):
     statements = re.findall(r'mul\([0-9]+,[0-9]+\)', prog)
     return sum(mult(s) for s in statements)
 
 
-assert run_it('xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))') == 161
+assert compute('xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))') == 161
 
 # Part 1
 
 part1_input = open('day3.txt').read()
-print(run_it(part1_input))
+print(compute(part1_input))
