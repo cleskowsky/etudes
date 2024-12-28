0e7dc0fb2ca095db066fd7e5a3a0482a6b2707b4
Christian Leskowsky <christian@leskowsky.net>
2024-12-27 21:34:10 -0500

    Appears to work with sample

--- adventofcode/2024py/day4.py
+++ adventofcode/2024py/day4.py
@@ -9,9 +9,7 @@ SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX"""
-
-the_grid = {}
-print(type(the_grid))
+print(sample_input)
 
 
 def parse_grid(s):
@@ -30,3 +28,33 @@ def parse_row(s, row_number, g):
 
 g = parse_grid(sample_input)
 print(g)
+
+# NW, N, NE
+# W,   , E
+# SW, S, SE
+headings = [
+    (-1, -1), (0, -1), (1, -1),
+    (-1, 0), (1, 0),
+    (-1, 1), (0, 1), (1, 1)
+]
+
+
+def find_word(pt, h, g, len=4):
+    s = ''
+    try:
+        for i in range(0, len):
+            next_char = g[(pt[0] + h[0] * i,
+                           pt[1] + h[1] * i)]
+            s = s + next_char
+    except KeyError:
+        return False
+    return s == 'XMAS'
+
+
+x = 0
+for k, v in g.items():
+    if v == 'X':
+        for h in headings:
+            if find_word(k, h, g):
+                x += 1
+print(x)
