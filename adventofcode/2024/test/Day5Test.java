8f77f6a2a3ef6957cf068e691e869f24c6c9e871
Christian Leskowsky <cleskowsky@oceanmd.com>
2024-12-29 07:24:51 -0500

    Added day5 test class

---
+++ adventofcode/2024/test/Day5Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class Day5Test {
+
+    @Test
+    public void goodOrdering() {
+        fail();
+    }
+}
