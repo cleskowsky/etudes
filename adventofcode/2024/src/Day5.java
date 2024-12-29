f1779e27e565f4d3edfd4c4f5478116fb7c93ab6
Christian Leskowsky <cleskowsky@oceanmd.com>
2024-12-29 12:12:54 -0500

    Start filling in goodOrdering test

--- adventofcode/2024/src/Day5.java
+++ adventofcode/2024/src/Day5.java
@@ -1,5 +1,22 @@
+import java.util.List;
+
 public class Day5 {
     public static void main(String[] args) {
         System.out.println(1);
+    }
+
+    static record PageOrderRule(int lhs, int rhs) {
+    }
+
+    static record Update(List<Integer> pages) {
+    }
+
+    static class Parser {
+        ParserResult parse(String s) {
+            return null;
+        }
+
+        record ParserResult(List<PageOrderRule> pageOrderRules, List<Update> updates) {
+        }
     }
 }
--- adventofcode/2024/test/Day5Test.java
+++ adventofcode/2024/test/Day5Test.java
@@ -6,7 +6,12 @@ class Day5Test {
 
     @Test
     public void goodOrdering() {
-        // Given a set of page rules and a manual update ordering
+        // Given a set of page pageOrderRules and a manual update ordering
+        var parser = new Day5.Parser();
+        Day5.Parser.ParserResult parserResult = parser.parse("");
+        var pageOrderRules = parserResult.pageOrderRules();
+        var manualUpdates = parserResult.updates();
+
         // When the ordering is validated
         // Then it is determined to be good
         fail();
