5ad806961b9705d2549a26c5457764915934436f
cleskowsky <christian@leskowsky.net>
2025-02-08 17:47:21 -0500

    Day6 (#6)
    Day 6 part 1, part 2

---
+++ adventofcode/2024/src/net/leskowsky/Day6.java
@@ -0,0 +1,132 @@
+package net.leskowsky;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Day6 {
+    public static void main(String[] args) {
+    }
+
+    // Lab
+    static record Lab(Map<Point, Boolean> floor) {
+        public boolean isBlocked(int x, int y) {
+            return isBlocked(new Point(x, y));
+        }
+
+        public boolean isBlocked(Point p) {
+            return floor.get(p);
+        }
+
+        public boolean contains(Point point) {
+            return floor.get(point) != null;
+        }
+
+        // Copy constructor
+        public Lab(Lab lab) {
+            this(new HashMap<>());
+            for (Point p : lab.floor().keySet()) {
+                floor.put(p, lab.floor().get(p));
+            }
+        }
+
+        /**
+         * Return number of blocked squares in lab
+         */
+        public List<Point> blocked() {
+            var result = new ArrayList<Point>();
+            for (var entry : floor.entrySet()) {
+                if (floor.get(entry.getKey())) {
+                    result.add(entry.getKey());
+                }
+            }
+            return result;
+        }
+
+        public void obstruct(Point p) {
+            floor.put(p, true);
+        }
+    }
+
+    enum Direction {
+        UP(0, -1),
+        RIGHT(1, 0),
+        DOWN(0, 1),
+        LEFT(-1, 0);
+
+        final int x;
+        final int y;
+
+        Direction(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static record SeenFacing(Point pos, Direction dir) {
+    }
+
+    // Guard
+    @Data
+    static class Guard {
+        Point pos;
+        Direction dir;
+
+        Set<Point> seen = new HashSet<>();
+        boolean leftLab;
+
+        Set<SeenFacing> seenFacings = new HashSet<>();
+        boolean looped;
+
+        public Guard(Point pos, Direction dir) {
+            this.pos = pos;
+            this.dir = dir;
+            seen.add(pos);
+        }
+
+        public void step(Lab lab) {
+            var next = new Point(pos.x() + dir.x, pos.y() + dir.y);
+
+            if (lab.contains(next)) {
+                while (lab.isBlocked(next)) {
+                    turn();
+                    next = new Point(pos.x() + dir.x, pos.y() + dir.y);
+                }
+
+                var seenFacing = new SeenFacing(next, dir);
+                if (seenFacings.contains(seenFacing)) {
+                    looped = true;
+                } else {
+                    seenFacings.add(seenFacing);
+                }
+
+                pos = next;
+                seen.add(next);
+            } else {
+                leftLab = true;
+            }
+        }
+
+        private void turn() {
+            switch (dir) {
+                case UP:
+                    dir = Direction.RIGHT;
+                    break;
+                case RIGHT:
+                    dir = Direction.DOWN;
+                    break;
+                case DOWN:
+                    dir = Direction.LEFT;
+                    break;
+                case LEFT:
+                    dir = Direction.UP;
+                    break;
+            }
+        }
+    }
+}
---
+++ adventofcode/2024/test/net/leskowsky/Day6Test.java
@@ -0,0 +1,217 @@
+package net.leskowsky;
+
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+import static net.leskowsky.Day6.Direction;
+import static net.leskowsky.Day6.Guard;
+import static net.leskowsky.Day6.Lab;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Day6Test {
+
+    // parse lab floor
+    @Test
+    void canParseInput() {
+        var s = """
+                ..
+                .#
+                """;
+        var result = InputParser.parse(s);
+        var lab = result.getLab();
+        assertEquals(4, lab.floor().size());
+        assertFalse(lab.isBlocked(0, 0));
+        assertTrue(lab.isBlocked(1, 1));
+    }
+
+    // walks forward
+    @Test
+    void guardStep() {
+        // Given a lab and a guard
+        var s = """
+                .
+                ^
+                """;
+        var result = InputParser.parse(s);
+        var g = result.getGuard();
+        assertEquals(new Point(0, 1), g.getPos());
+        assertEquals(Direction.UP, g.getDir());
+
+        // When the guard takes a step
+        g.step(result.getLab());
+
+        // His has moved up and his heading hasn't changed
+        assertEquals(new Point(0, 0), g.getPos());
+        assertEquals(Direction.UP, g.getDir());
+    }
+
+    // turns right to avoid obstacles
+    @Test
+    void guardTurnsToAvoidObstacle() {
+        // Given an obstacle in front of the guard
+        var s = """
+                #.
+                ^.""";
+        var result = InputParser.parse(s);
+        var g = result.getGuard();
+
+        // When the guard steps
+        g.step(result.getLab());
+
+        // He turns right then moves
+        assertEquals(new Point(1, 1), g.getPos());
+        assertEquals(Direction.RIGHT, g.getDir());
+    }
+
+    // walks outside mapped area
+    @Test
+    void guardLeavesFloor() {
+        // Given a guard at the floor's edge
+        var s = """
+                ^.
+                ..""";
+        var result = InputParser.parse(s);
+        var g = result.getGuard();
+
+        // When the guard takes a step outside the floor
+        g.step(result.getLab());
+
+        // He doesn't move or turn (Simulator is finished)
+        assertEquals(new Point(0, 0), g.getPos());
+        assertEquals(Direction.UP, g.getDir());
+    }
+
+    // remembers path taken
+    @Test
+    void stepLog() {
+        // Given a guard and a floor
+        String s = """
+                .#
+                ^.""";
+        var result = InputParser.parse(s);
+        var g = result.getGuard();
+
+        // When the guard takes a step
+        while (true) {
+            g.step(result.getLab());
+            if (g.isLeftLab()) {
+                continue;
+            }
+            break;
+        }
+
+        // Then a record of squares occupied is kept
+        assertEquals(2, g.getSeen().size());
+    }
+
+    @Test
+    void sample() throws IOException {
+        var result = InputParser.parseFile("inputs/day6_sample.txt");
+
+        var g = result.getGuard();
+        while (true) {
+            g.step(result.getLab());
+            if (g.isLeftLab()) {
+                continue;
+            }
+            break;
+        }
+
+        System.out.println(g.getSeen().size());
+    }
+
+    @Test
+    void part1() throws IOException {
+        var result = InputParser.parseFile("inputs/day6.txt");
+
+        var g = result.getGuard();
+        while (true) {
+            g.step(result.getLab());
+            if (g.isLeftLab()) {
+                break;
+            }
+        }
+
+        assertEquals(5404, g.getSeen().size());
+    }
+
+    @Test
+    void part2() throws IOException {
+        var result = InputParser.parseFile("inputs/day6.txt");
+
+        int cnt = 0;
+
+        // for each open square
+        var lab = result.getLab();
+        for (Point p : lab.floor().keySet()) {
+            // add obstruction
+            var newLab = new Lab(lab);
+            newLab.obstruct(p);
+
+            // reset guard and simulate guard movement
+            var g = new Guard(
+                    new Point(result.getGuard().getPos()),
+                    Direction.UP
+            );
+            while (true) {
+                g.step(newLab);
+                if (g.isLeftLab()) {
+                    // if we exit map, stop
+                    break;
+                } else if (g.isLooped()) {
+                    // if we've been here before we found a loop
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        // 1959 is too low
+        assertEquals(1984, cnt);
+    }
+
+    static class InputParser {
+        @Data
+        static class ParseResult {
+            Lab lab;
+            Guard guard;
+        }
+
+        static ParseResult parse(String s) {
+            var result = new ParseResult();
+
+            var floor = new HashMap<Point, Boolean>();
+            var lab = new Lab(floor);
+            result.setLab(lab);
+
+            var rows = s.split("\n");
+            for (int i = 0; i < rows.length; i++) {
+                var row = rows[i].toCharArray();
+                for (int j = 0; j < row.length; j++) {
+                    if (row[j] == '#') {
+                        floor.put(new Point(j, i), true);
+                    } else if (row[j] == '^') {
+                        result.setGuard(new Guard(new Point(j, i), Direction.UP));
+                        // guard start square is not blocked
+                        floor.put(new Point(j, i), false);
+                    } else {
+                        floor.put(new Point(j, i), false);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static ParseResult parseFile(String s) throws IOException {
+            return parse(Files.readString(Path.of(s)));
+        }
+    }
+}
