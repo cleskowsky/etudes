5d75a3f72f81cef4748a04869255812ecbe49534
Christian Leskowsky <christian@leskowsky.net>
2025-02-09 17:01:56 -0500

    Part 1

--- adventofcode/2024/src/net/leskowsky/Day7.java
+++ adventofcode/2024/src/net/leskowsky/Day7.java
@@ -1,39 +1,52 @@
 package net.leskowsky;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day7 {
-    public static void main(String[] args) {
-
-//        Sample input
-//        190: 10 19
-//        3267: 81 40 27
-//        83: 17 5
-//        156: 15 6
-//        7290: 6 8 6 15
-//        161011: 16 10 13
-//        192: 17 8 14
-//        21037: 9 7 18 13
-//        292: 11 6 16 20
+    public static void main(String[] args) throws IOException {
 
         var x = new Day7();
-//        x.solver(List.of("10", "19"), 0, new ArrayList<>(), 190);
-//        x.solver(List.of("81", "40", "27"), 0, new ArrayList<>(), 3267);
-//        x.solver(List.of("11", "6", "16", "20"), 0, new ArrayList<>(), 292);
 
-//        190: 10 19
-//        x.solver(10, List.of(19), 190);
-        x.solver(11, List.of(6, 16, 20), 292);
+        assert x.solver(81, List.of(40, 27), 3267);
+        assert !x.solver(16, List.of(10, 13), 161011);
+
+        // sample input
+//        String input = Files.readString(Path.of("inputs/day7_sample.txt"));
+        String input = Files.readString(Path.of("inputs/day7.txt"));
+
+        long sum = 0;
+
+        for (String line : input.split("\n")) {
+            var split = line.split(": ");
+            long answer = Long.parseLong(split[0]);
+            List<Integer> terms = Arrays.stream(split[1].split(" "))
+                    .map(Integer::parseInt)
+                    .toList();
+            if (x.solver(terms.getFirst(),
+                    terms.subList(1, terms.size()),
+                    answer)) {
+                sum += answer;
+            }
+        }
+
+        System.out.println(sum);
     }
 
-    public void solver(long lhs, List<Integer> terms, long answer) {
-        if (terms.isEmpty()) {
+    public boolean solver(long lhs, List<Integer> theRest, long answer) {
+        if (theRest.isEmpty()) {
+//            System.out.printf("lhs=%d answer=%d\n", lhs, answer);
             // is lhs == answer
-            System.out.printf("lhs=%d answer=%d good_eq=%b\n", lhs, answer, lhs==answer);
+            return lhs == answer;
         } else {
-            var t = terms.getFirst();
-            solver(lhs + t, terms.subList(1, terms.size()), answer);
-            solver(lhs * t, terms.subList(1, terms.size()), answer);
+            var t = theRest.getFirst();
+            // try add
+            return solver(lhs + t, theRest.subList(1, theRest.size()), answer) ||
+                    // try mult
+                    solver(lhs * t, theRest.subList(1, theRest.size()), answer);
         }
     }
 }
