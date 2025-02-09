f5f62ca04d594d4f1b5dfe596b8c991dfe090b47
Christian Leskowsky <christian@leskowsky.net>
2025-02-09 16:12:41 -0500

    Added a few more test cases to solver

--- adventofcode/2024/src/net/leskowsky/Day7.java
+++ adventofcode/2024/src/net/leskowsky/Day7.java
@@ -18,28 +18,53 @@ public class Day7 {
 //        292: 11 6 16 20
 
         var x = new Day7();
-        x.solver(List.of("10", "19"), 0, new ArrayList<>());
+//        x.solver(List.of("10", "19"), 0, new ArrayList<>(), 190);
+//        x.solver(List.of("81", "40", "27"), 0, new ArrayList<>(), 3267);
+        x.solver(List.of("11", "6", "16", "20"), 0, new ArrayList<>(), 292);
     }
 
-    public void solver(List<String> terms, int i, List<String> expr) {
+    public void solver(List<String> terms, int i, List<String> expr, long answer) {
         int x = terms.size() - i;
 
         if (x == 0) {
+            // final expression to calculate
             System.out.println(expr);
+
+            // calculate expr
+            int lhs = Integer.parseInt(expr.getFirst());
+
+            for (int j = 1; j < expr.size(); j += 2) {
+                var op = expr.get(j).charAt(0);
+                int rhs = Integer.parseInt(expr.get(j + 1));
+                if (op == '+') {
+                    lhs += rhs;
+                } else if (op == '*') {
+                    lhs *= rhs;
+                }
+            }
+            // accumulated result
+            System.out.println(lhs);
+
+            // does our result match answer
+            if (lhs == answer) {
+                System.out.println("Good equation " + expr);
+            }
         } else if (x == 1) {
+            // last term
             var addExpr = new ArrayList<>(expr);
             addExpr.add(terms.get(i));
-            solver(terms, i + 1, addExpr);
+            solver(terms, i + 1, addExpr, answer);
         } else {
             // try add
             var addExpr = new ArrayList<>(expr);
             addExpr.addAll(List.of(terms.get(i), "+"));
-            solver(terms, i + 1, addExpr);
+            solver(terms, i + 1, addExpr, answer);
 
             // try mult
             var multExpr = new ArrayList<>(expr);
             multExpr.addAll(List.of(terms.get(i), "*"));
-            solver(terms, i + 1, multExpr);
+            solver(terms, i + 1, multExpr, answer);
         }
     }
 }
+
