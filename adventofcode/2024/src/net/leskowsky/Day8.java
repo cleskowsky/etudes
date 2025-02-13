4d44f6eb5d20865e2f10e9e6e85145605d636d81
Christian Leskowsky <christian@leskowsky.net>
2025-02-13 09:05:25 -0500

    Added day8 scaffold

---
+++ adventofcode/2024/src/net/leskowsky/Day8.java
@@ -0,0 +1,7 @@
+package net.leskowsky;
+
+public class Day8 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
---
+++ adventofcode/2024/test/net/leskowsky/Day8Test.java
@@ -0,0 +1,13 @@
+package net.leskowsky;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day8Test {
+
+    @Test
+    void part1() {
+        fail();
+    }
+}
