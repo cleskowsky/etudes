9f01f2d0540387250547f05113791e3198d69cc8
Christian Leskowsky <christian@leskowsky.net>
2025-02-16 08:49:50 -0500

    Finished parser

--- adventofcode/2024/test/net/leskowsky/Day8Test.java
+++ adventofcode/2024/test/net/leskowsky/Day8Test.java
@@ -12,11 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class Day8Test {
 
     @Test
-    void part1() {
-        fail();
-    }
-
-    @Test
     void parseInput() {
         String s = """
                 .a
@@ -24,9 +19,11 @@ class Day8Test {
 
         assertEquals(1, parseMap(s).signals().size());
         assertEquals(2, parseMap(s).signals().get('a').size());
+        assertEquals(2, parseMap(s).rows());
+        assertEquals(2, parseMap(s).cols());
     }
 
-    record ParseResult(Map<Character, List<Point>> signals) {
+    record ParseResult(Map<Character, List<Point>> signals, int rows, int cols) {
     }
 
     private ParseResult parseMap(String s) {
@@ -47,8 +44,18 @@ class Day8Test {
                 signals.put(c, x);
             }
         }
-        System.out.println(signals);
 
-        return new ParseResult(signals);
+        return new ParseResult(signals, lines[0].length(), lines.length);
+    }
+
+    @Test
+    void part1() {
+        // find signals
+        // foreach signal
+        //   find pairs
+        //   foreach pair
+        //     find antinodes
+        // count antinodes
+        assertTrue(false);
     }
 }
