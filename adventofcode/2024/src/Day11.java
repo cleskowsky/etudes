f422d5563b4f226a2e663ef67ef0f39716ce7139
Christian Leskowsky <christian@leskowsky.net>
2025-03-13 06:51:10 -0400

    Refactor blink method to accept rounds parameter and update part1 example

--- adventofcode/2024/src/Day11.java
+++ adventofcode/2024/src/Day11.java
@@ -14,11 +14,7 @@ public class Day11 {
         System.out.println("example");
 
         var stones = List.of(125l, 17l);
-        for (int i = 0; i < 25; i++) {
-            stones = blink(stones);
-            // System.out.println(stones);
-        }
-        System.out.println(stones.size());
+        System.out.println(blink(stones, 6));
     }
 
     /**
@@ -26,42 +22,53 @@ public class Day11 {
      * 
      * @param s
      */
-    List<Long> blink(List<Long> stones) {
-        var result = new ArrayList<Long>();
+    int blink(List<Long> stones, int rounds) {
 
-        for (var s : stones) {
-            // 0 becomes 1
-            if (s == 0) {
-                result.add(1l);
-                continue;
+        var val = new ArrayList<Long>(stones);
+
+        for (int i = 0; i < rounds; i++) {
+
+            // a scratch list to store stones after each round
+            var result = new ArrayList<Long>();
+
+            for (var s : val) {
+                // 0 becomes 1
+                if (s == 0) {
+                    result.add(1l);
+                    continue;
+                }
+
+                // even number of digits - splits into 2
+                if (s.toString().length() % 2 == 0) {
+                    var str = s.toString();
+                    var half = str.length() / 2;
+                    var first = Long.parseLong(str.substring(0, half));
+                    var second = Long.parseLong(str.substring(half));
+                    result.add(first);
+                    result.add(second);
+                    continue;
+                }
+
+                // default - *2024
+                result.add(s * 2024);
             }
 
-            // even number of digits - splits into 2
-            if (s.toString().length() % 2 == 0) {
-                var str = s.toString();
-                var half = str.length() / 2;
-                var first = Long.parseLong(str.substring(0, half));
-                var second = Long.parseLong(str.substring(half));
-                result.add(first);
-                result.add(second);
-                continue;
+            if (debug) {
+                System.out.println(result);
             }
 
-            // default - *2024
-            result.add(s * 2024);
+            val = result;
         }
 
-        return result;
+        return val.size();
     }
+
+    static boolean debug = false;
 
     void part1() {
         System.out.println("part 1");
 
         var stones = List.of(5l, 89749l, 6061l, 43l, 867l, 1965860l, 0l, 206250l);
-        for (int i = 0; i < 25; i++) {
-            stones = blink(stones);
-        }
-        System.out.println(stones.size());
-
+        System.out.println(blink(stones, 25));
     }
 }
