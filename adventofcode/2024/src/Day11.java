2933f0259e16740f6408f08af8ebf550e9693eb3
Christian Leskowsky <christian@leskowsky.net>
2025-03-13 05:51:29 -0400

    Implement part1 method and update example in Day11

--- adventofcode/2024/src/Day11.java
+++ adventofcode/2024/src/Day11.java
@@ -7,12 +7,18 @@ public class Day11 {
 
         var d = new Day11();
         d.example();
+        d.part1();
     }
 
     void example() {
         System.out.println("example");
+
         var stones = List.of(125l, 17l);
-        System.out.println(blink(stones));
+        for (int i = 0; i < 25; i++) {
+            stones = blink(stones);
+            // System.out.println(stones);
+        }
+        System.out.println(stones.size());
     }
 
     /**
@@ -29,10 +35,33 @@ public class Day11 {
                 result.add(1l);
                 continue;
             }
+
+            // even number of digits - splits into 2
+            if (s.toString().length() % 2 == 0) {
+                var str = s.toString();
+                var half = str.length() / 2;
+                var first = Long.parseLong(str.substring(0, half));
+                var second = Long.parseLong(str.substring(half));
+                result.add(first);
+                result.add(second);
+                continue;
+            }
+
+            // default - *2024
+            result.add(s * 2024);
         }
 
-        // even number of digits - splits into 2
-        // default - x2024
         return result;
+    }
+
+    void part1() {
+        System.out.println("part 1");
+
+        var stones = List.of(5l, 89749l, 6061l, 43l, 867l, 1965860l, 0l, 206250l);
+        for (int i = 0; i < 25; i++) {
+            stones = blink(stones);
+        }
+        System.out.println(stones.size());
+
     }
 }
