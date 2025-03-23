b38fcb7fed7063ac65858e8bb4892da2a289bfb8
Christian Leskowsky <christian@leskowsky.net>
2025-03-23 07:41:32 -0400

    Implement stone blinking logic with recursive splitting behavior
    Added functionality to process stones through recursive "blinking" rounds. Handles splitting logic based on digit count and applies transformations like multiplication or splitting. Existing `part1` method commented out for now, focusing on core blinking behavior.

--- adventofcode/2024/src/Day11.java
+++ adventofcode/2024/src/Day11.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day11 {
@@ -6,27 +7,69 @@ public class Day11 {
 
         var d = new Day11();
         d.example();
-        d.part1();
+//        d.part1();
     }
 
     void example() {
         System.out.println("example");
 
-        var stones = List.of(125l, 17l);
+        var stones = List.of(125L, 17L);
         System.out.println(blink(stones, 6));
     }
 
     // Return stones after n blinks
     int blink(List<Long> stones, int rounds) {
-        return 0;
+
+        var val = new ArrayList<Long>(stones);
+
+        for (int i = 0; i < rounds; i++) {
+
+            // a scratch list to store stones after each round
+            var result = new ArrayList<Long>();
+
+            for (var s : val) {
+                var x = blink(s);
+                result.addAll(x);
+            }
+
+            if (debug) {
+                System.out.println(result);
+            }
+
+            val = result;
+        }
+
+        return val.size();
+    }
+
+    private static List<Long> blink(Long s) {
+        var result = new ArrayList<Long>();
+
+        // 0 becomes 1
+        if (s == 0) {
+            result.add(1L);
+        } else if (s.toString().length() % 2 == 0) {
+            // even number of digits - splits into 2
+            var str = s.toString();
+            var half = str.length() / 2;
+            var first = Long.parseLong(str.substring(0, half));
+            var second = Long.parseLong(str.substring(half));
+            result.add(first);
+            result.add(second);
+        } else {
+            // default - *2024
+            result.add(s * 2024);
+        }
+
+        return result;
     }
 
     static boolean debug = false;
 
-    void part1() {
-        System.out.println("part 1");
-
-        var stones = List.of(5l, 89749l, 6061l, 43l, 867l, 1965860l, 0l, 206250l);
-        System.out.println(blink(stones, 25));
-    }
+//    void part1() {
+//        System.out.println("part 1");
+//
+//        var stones = List.of(5l, 89749l, 6061l, 43l, 867l, 1965860l, 0l, 206250l);
+//        System.out.println(blink(stones, 25));
+//    }
 }
