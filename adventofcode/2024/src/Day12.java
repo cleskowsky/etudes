c7821b67ae2b8a7c13ba59e63a38eb1499bbd078
Christian Leskowsky <christian@leskowsky.net>
2025-03-31 06:45:27 -0400

    Started with a few tiny types and a method to play with the example

--- adventofcode/2024/src/Day12.java
+++ adventofcode/2024/src/Day12.java
@@ -1,5 +1,48 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class Day12 {
     public static void main(String[] args) {
         System.out.println(1);
+
+        var d = new Day12();
+        d.example();
+    }
+
+    public void example() {
+        System.out.println("example");
+
+        var s = """
+                AAAA
+                BBCD
+                BBCC
+                EEEC""";
+        System.out.println(s);
+
+        Farm farm = parseInput(s);
+
+        var total = farm.getRegions().stream()
+            .map(Day12::fencePrice)
+            .reduce(0, Integer::sum);
+    }
+
+    class Farm extends HashMap<Point, Character> {
+        public List<Region> getRegions() {
+            return null;
+        }
+    }
+
+    record Region(List<Character> plots) {
+    }
+
+    record Point(int x, int y) {
+    }
+
+    public static int fencePrice(Region region) {
+        return 0;
+    }
+
+    Farm parseInput(String s) {
+        return null;
     }
 }
