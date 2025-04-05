2784ba31f018e7943df3bf8098f276f3c98e1cd9
Christian Leskowsky <christian@leskowsky.net>
2025-04-05 06:12:37 -0400

    Removed target class from repo

---
+++ adventofcode/2024/src/test/java/Day12Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class Day12Test {
+
+    @Test
+    void thisTestShouldFail() {
+        // fail("Not yet implemented");
+        assertTrue(true);
+    }
+}
