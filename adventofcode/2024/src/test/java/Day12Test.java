b9877f292b9b79dbd20e71707ba2099845ca6045
Christian Leskowsky <christian@leskowsky.net>
2025-04-05 06:20:21 -0400

    Update .gitignore to include lib and target directories; modify pom.xml build section; clean up Day12Test.java imports and remove fail statement
    Lombok dep should be downloaded for all targets

--- adventofcode/2024/src/test/java/Day12Test.java
+++ adventofcode/2024/src/test/java/Day12Test.java
@@ -1,13 +1,11 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
 
 public class Day12Test {
 
     @Test
     void thisTestShouldFail() {
-        // fail("Not yet implemented");
         assertTrue(true);
     }
 }
