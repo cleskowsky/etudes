1ce12353d2fb9a5e99446655b1cf62b139e95bb2
Christian Leskowsky <christian@leskowsky.net>
2025-04-27 08:53:23 -0400

    First

---
+++ adventofcode/2024/test/Day12Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class Day12Test {
+
+    @Test
+    void parseInput() {
+        var s = "input_string";
+        Day12.InputParser.parse(s);
+    }
+
+    // another test
+    // ...
+}
