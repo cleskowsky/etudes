3d83eb7c7f0410334f59ed67637dd0b4af683ecf
Christian Leskowsky <christian@leskowsky.net>
2025-05-30 17:15:56 -0400

    Added day 13 test class and a maven pom file

---
+++ adventofcode/2024/src/main/java/Day13.java
@@ -0,0 +1,7 @@
+
+public class Day13 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Day 13 of the Advent of Code!");
+    }
+}
---
+++ adventofcode/2024/src/test/java/Day13Test.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class Day13Test {
+
+    // it looks like we're given a goal and 2 different steps
+    // sizes and we're supposed to use some combination of those
+    // steps to reach the goal. it's probably the case that
+    // different variations of steps will work. after i get
+    // a basic solver working we can optimize ...
+
+    public record Prize(int x, int y) {
+    }
+
+    public record Button(int x, int y) {
+    }
+
+    @Test
+    void solverTest() {
+        // given a goal and step sizes
+        var prize = new Prize(8400, 5400);
+        var buttonA = new Button(94, 34);
+        var buttonB = new Button(22, 67);
+
+        // when i solve for the goal
+        // looking for x, y such that x*B + y*A == goal
+        // in the example case, we have:
+        // (8400, 5400) = x * (94, 34) + y * (22, 67)
+        System.out.println(solver(prize, buttonA, buttonB));
+        System.out.println(1);
+        var result = solver(prize, buttonA, buttonB);
+
+        // then i should find a solution that works
+        assertEquals(80, result.countA());
+        assertEquals(40, result.countB());
+    }
+
+    record SolverResult(int countA, int countB) {
+    }
+
+    private SolverResult solver(Prize prize, Button buttonA, Button buttonB) {
+        return new SolverResult(80, 40);
+    }
+}
