cf3d5bc3af52731f5aecc1c47d9c1ce0e94ade28
Christian Leskowsky <christian@leskowsky.net>
2025-05-31 06:10:43 -0400

    Moved logic to day13 class

--- adventofcode/2024/src/main/java/Day13.java
+++ adventofcode/2024/src/main/java/Day13.java
@@ -1,5 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 
 public class Day13 {
+
+    public record Prize(int x, int y) {
+    }
+
+    public record Button(int x, int y) {
+    }
+
+    public record SolverResult(int countA, int countB) {
+    }
+
+    record Machine(Prize p, Button a, Button b) {
+    }
 
     // Returns the cheapest presses of buttons a, b to reach the prize
     // Pressing button a costs 3 credits
@@ -18,5 +37,46 @@ public class Day13 {
         }
 
         return result;
+    }
+
+    // Example input:
+    //    Button A: X+40, Y+38
+    //    Button B: X+21, Y+84
+    //    Prize: X=4245, Y=5634
+    //
+    //    Button A: X+19, Y+11
+    //    Button B: X+37, Y+98
+    //    Prize: X=3246, Y=6474
+    //    ...
+    private List<Machine> parseInput(String s) {
+        var result = new ArrayList<Machine>();
+
+        try {
+            var lines = Files.readString(Path.of(s)).split("\n");
+            for (int i = 0; i < lines.length; i += 4) {
+                var b1 = parseInts(lines[i]);
+                var b2 = parseInts(lines[i + 1]);
+                var p = parseInts(lines[i + 2]);
+                result.add(
+                        new Machine(
+                                new Prize(p.getFirst(), p.getLast()),
+                                new Button(b1.getFirst(), b1.getLast()),
+                                new Button(b2.getFirst(), b2.getLast())
+                        )
+                );
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+    private List<Integer> parseInts(String s) {
+        var m = Pattern.compile("\\d{2}");
+        return m.matcher(s).results()
+                .map(MatchResult::group)
+                .map(Integer::parseInt)
+                .toList();
     }
 }
--- adventofcode/2024/src/test/java/Day13Test.java
+++ adventofcode/2024/src/test/java/Day13Test.java
@@ -1,12 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.MatchResult;
-import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,15 +11,6 @@ public class Day13Test {
     // steps to reach the goal. it's probably the case that
     // different variations of steps will work. after i get
     // a basic solver working we can optimize ...
-
-    public record Prize(int x, int y) {
-    }
-
-    public record Button(int x, int y) {
-    }
-
-    public record SolverResult(int countA, int countB) {
-    }
 
     @Test
     void solver() {
@@ -66,53 +51,5 @@ public class Day13Test {
         System.out.println(machines.size());
     }
 
-    // Example input:
-    //    Button A: X+40, Y+38
-    //    Button B: X+21, Y+84
-    //    Prize: X=4245, Y=5634
-    //
-    //    Button A: X+19, Y+11
-    //    Button B: X+37, Y+98
-    //    Prize: X=3246, Y=6474
-    //    ...
-    private List<Machine> parseInput(String s) {
-        var result = new ArrayList<Machine>();
 
-        try {
-            var lines = Files.readString(Path.of(s)).split("\n");
-            for (int i = 0; i < lines.length; i += 4) {
-                var b1 = parseInts(lines[i]);
-                var b2 = parseInts(lines[i + 1]);
-                var p = parseInts(lines[i + 2]);
-                result.add(
-                        new Machine(
-                                new Prize(p.getFirst(), p.getLast()),
-                                new Button(b1.getFirst(), b1.getLast()),
-                                new Button(b2.getFirst(), b2.getLast())
-                        )
-                );
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return result;
-    }
-
-    record Machine(Prize p, Button a, Button b) {
-    }
-
-    @Test
-    void testParseInts() {
-        var x = parseInts("Button A: X+40, Y+38");
-        assertEquals(new Button(40, 38), new Button(x.get(0), x.get(1)));
-    }
-
-    private List<Integer> parseInts(String s) {
-        var m = Pattern.compile("\\d{2}");
-        return m.matcher(s).results()
-                .map(MatchResult::group)
-                .map(Integer::parseInt)
-                .toList();
-    }
 }
