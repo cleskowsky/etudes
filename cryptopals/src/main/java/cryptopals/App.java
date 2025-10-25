b6f8ce1faa5d09bd4798911da73c0bcad1471208
Christian Leskowsky <christian@leskowsky.net>
2025-10-25 08:35:03 -0400

    Implement hex to binary conversion and update tests

--- cryptopals/src/main/java/cryptopals/App.java
+++ cryptopals/src/main/java/cryptopals/App.java
@@ -1,13 +1,57 @@
 package cryptopals;
 
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        var x = 0x4 << 4;
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(String.format("%8s",
+                Integer.toBinaryString(x)).replace(' ', '0'));
+    }
+
+    Map<Character, String> hexToBinaryMap = Map.ofEntries(
+            Map.entry('0', "0000"),
+            Map.entry('1', "0001"),
+            Map.entry('2', "0010"),
+            Map.entry('3', "0011"),
+            Map.entry('4', "0100"),
+            Map.entry('5', "0101"),
+            Map.entry('6', "0110"),
+            Map.entry('7', "0111"),
+            Map.entry('8', "1000"),
+            Map.entry('9', "1001"),
+            Map.entry('a', "1010"),
+            Map.entry('b', "1011"),
+            Map.entry('c', "1100"),
+            Map.entry('d', "1101"),
+            Map.entry('e', "1110"),
+            Map.entry('f', "1111"));
+
+    /**
+     * Convert hex string to binary
+     */
+    public String hexToBinary(String s) {
+
+        if (s.length() % 2 != 0) {
+            throw new IllegalArgumentException("Hex string must have even length");
+        }
+
+        var ret = new StringBuilder();
+        for (int i = 0; i < s.length(); i += 2) {
+            var c1 = s.charAt(i);
+            var c2 = s.charAt(i + 1);
+            ret.append(hexToBinaryMap.get(c1));
+            ret.append(hexToBinaryMap.get(c2));
+        }
+
+        return ret.toString();
     }
 }
--- cryptopals/src/test/java/cryptopals/AppTest.java
+++ cryptopals/src/test/java/cryptopals/AppTest.java
@@ -1,19 +1,30 @@
 package cryptopals;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    void shouldAnswerWithTrue() {
-        assertTrue(true);
+    void hexToBinary() {
+
+        var app = new App();
+
+        // base case
+        var test = "4b";
+        assertEquals("01001011", app.hexToBinary(test));
+
+        // fails on odd length
+        try {
+            app.hexToBinary("4");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Hex string must have even length", e.getMessage());
+        }
     }
 }
