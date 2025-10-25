3d7554e2a1d167378993ac66067245ceb192279b
Christian Leskowsky <christian@leskowsky.net>
2025-10-25 09:54:27 -0400

    Refactor hexToBinary method to toBinary, update tests for byte array input and error handling

--- cryptopals/src/main/java/cryptopals/App.java
+++ cryptopals/src/main/java/cryptopals/App.java
@@ -1,7 +1,5 @@
 package cryptopals;
 
-import java.util.Map;
-
 /**
  * Hello world!
  *
@@ -15,43 +13,23 @@ public class App {
         System.out.println(Integer.toBinaryString(x));
         System.out.println(String.format("%8s",
                 Integer.toBinaryString(x)).replace(' ', '0'));
+
+        System.out.println(Integer.toBinaryString(15));
     }
 
-    Map<Character, String> hexToBinaryMap = Map.ofEntries(
-            Map.entry('0', "0000"),
-            Map.entry('1', "0001"),
-            Map.entry('2', "0010"),
-            Map.entry('3', "0011"),
-            Map.entry('4', "0100"),
-            Map.entry('5', "0101"),
-            Map.entry('6', "0110"),
-            Map.entry('7', "0111"),
-            Map.entry('8', "1000"),
-            Map.entry('9', "1001"),
-            Map.entry('a', "1010"),
-            Map.entry('b', "1011"),
-            Map.entry('c', "1100"),
-            Map.entry('d', "1101"),
-            Map.entry('e', "1110"),
-            Map.entry('f', "1111"));
-
     /**
-     * Convert hex string to binary
+     * Converts hex to binary
      */
-    public String hexToBinary(String src) {
-
-        if (src.length() % 2 != 0) {
-            throw new IllegalArgumentException("Hex string should have even length");
+    public byte[] toBinary(byte[] src) {
+        if (src.length % 2 != 0) {
+            throw new IllegalArgumentException("byte[] src must have even length");
         }
 
-        var dst = new StringBuilder();
-        for (int i = 0; i < src.length(); i += 2) {
-            var c1 = src.charAt(i);
-            var c2 = src.charAt(i + 1);
-            dst.append(hexToBinaryMap.get(c1));
-            dst.append(hexToBinaryMap.get(c2));
+        var dst = new byte[src.length / 2];
+        for (int i = 0; i < src.length; i += 2) {
+            dst[i / 2] = (byte) (src[i] << 4 | src[i + 1]);
         }
 
-        return dst.toString();
+        return dst;
     }
 }
--- cryptopals/src/test/java/cryptopals/AppTest.java
+++ cryptopals/src/test/java/cryptopals/AppTest.java
@@ -3,6 +3,8 @@ package cryptopals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.ByteArrayOutputStream;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -16,15 +18,24 @@ public class AppTest {
         var app = new App();
 
         // base case
-        var test = "4b";
-        assertEquals("01001011", app.toBinary(test));
+        var t1 = "4b";
+
+        var buf = new ByteArrayOutputStream();
+        for (char c : t1.toCharArray()) {
+            buf.write(Character.digit(c, 16));
+        }
+
+        var retval = app.toBinary(buf.toByteArray());
+        // with 0-padding expect 01001011
+        // which is the same as 1001011
+        assertEquals("1001011", Integer.toString(retval[0], 2));
 
         // fails on odd length
         try {
-            app.toBinary("4");
+            app.toBinary(new byte[] { 4 });
             fail();
         } catch (IllegalArgumentException e) {
-            assertEquals("Hex string must have even length", e.getMessage());
+            assertEquals("byte[] src must have even length", e.getMessage());
         }
     }
 }
