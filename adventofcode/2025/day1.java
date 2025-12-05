35157c5423c3c7f25d4abb0f0fadfb7d7cff031b
Christian Leskowsky <christian@leskowsky.net>
2025-12-05 17:48:59 -0500

    Refactor Day 1 solution: streamline logic and improve readability

--- adventofcode/2025/day1.java
+++ adventofcode/2025/day1.java
@@ -1,31 +1,28 @@
 import java.io.IOException;
 
 void main() throws IOException {
+    var sample = Files.readAllLines(Path.of("inputs/day1_sample.txt"));
     var turns = Files.readAllLines(Path.of("inputs/day1.txt"));
-    // var turns = Files.readAllLines(Path.of("inputs/day1_sample.txt"));
 
-    int dial = 50;
+    // part 1
+    assert 3 == passwd(50, sample);
+    assert 1123 == passwd(50, turns);
+
+    // part 2
+    assert 6 == passwd2(50, sample);
+    assert 6695 == passwd2(50, turns);
+}
+
+// Counts number of times dial is zero after turn
+int passwd(int dial, List<String> turns) {
     int passwd = 0;
-    int zeros = 0;
 
     for (var turn : turns) {
-        var clicks = Integer.parseInt(turn.substring(1));
+        var ticks = Integer.parseInt(turn.substring(1));
         if (turn.startsWith("L")) {
-            for (int i = 0; i < clicks; i++) {
-                dial -= 1;
-                if (dial % 100 == 0) {
-                    zeros++;
-                }
-            }
-        } else if (turn.startsWith("R")) {
-            for (int i = 0; i < clicks; i++) {
-                dial += 1;
-                if (dial % 100 == 0) {
-                    zeros++;
-                }
-            }
+            dial -= ticks;
         } else {
-            throw new RuntimeException("Bad turn direction: " + turn.substring(0, 1));
+            dial += ticks;
         }
 
         dial = dial % 100;
@@ -35,6 +32,24 @@ void main() throws IOException {
         }
     }
 
-    System.out.println(passwd);
-    System.out.println(zeros);
+    return passwd;
+}
+
+// Counts number of times dial crosses 0 across turns
+int passwd2(int dial, List<String> turns) {
+    int zeros = 0;
+
+    for (var turn : turns) {
+        var ticks = Integer.parseInt(turn.substring(1));
+        var incr = turn.startsWith("R") ? 1 : -1;
+
+        for (int i = 0; i < ticks; i++) {
+            dial += incr;
+            if (dial % 100 == 0) {
+                zeros++;
+            }
+        }
+    }
+
+    return zeros;
 }
