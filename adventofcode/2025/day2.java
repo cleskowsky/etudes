d3a64e8c12856a5010a9212d6b65bf5ffc932c73
Christian Leskowsky <christian@leskowsky.net>
2025-12-07 10:42:49 -0500

    Have to find a bug in isValid

--- adventofcode/2025/day2.java
+++ adventofcode/2025/day2.java
@@ -2,30 +2,48 @@ import static java.lang.IO.println;
 
 void main() throws IOException {
 
-    var checker = new DefaultChecker();
+    var v = new DefaultValidator();
 
-    assert checker.check(1188511880);
-    assert !checker.check(1188511885);
+    assert v.isValid(1188511880);
+    assert !v.isValid(1188511885);
 
-    var t1 = invalidIds(new Range(1188511880, 1188511890), checker);
+    var t1 = invalidIds(new Range(1188511880, 1188511890), v);
     assert 1 == t1.size();
     assert 1188511885 == t1.getFirst();
 
-    assert checker.check(101);
+    assert v.isValid(101);
 
     List<Range> sampleRanges = parse("inputs/day2_sample.txt");
     List<Range> ranges = parse("inputs/day2.txt");
 
     partA(sampleRanges);
     partA(ranges);
+
+    // in part b an id is invalid if it is made up of the same pattern
+    // repeated at least twice
+    // eg 11 is '1' repeated 2 times
+    //    999 is '9' repeated 3 times
+    //    824824824 is '824' repeated 3 times
+    var v2 = new RepeatValidator();
+
+    assert 2 == v2.split("11", 1).size();
+    assert 1 == v2.split("11", 2).size();
+    assert 2 == v2.split("111", 2).size();
+    assert 3 == v2.split("824824824", 3).size();
+
+    // 1 is repeated twice
+    assert !v2.isValid(11);
+//    assert !v2.isValid(824824824);
+
+//    partB(sampleRanges);
 }
 
-List<Long> invalidIds(Range r, Checker checker) {
+List<Long> invalidIds(Range r, Validator v) {
     var val = new ArrayList<Long>();
     for (long i = r.min(); i <= r.max(); i++) {
 
         // skip good ids
-        if (checker.check(i)) {
+        if (v.isValid(i)) {
             continue;
         }
 
@@ -35,11 +53,11 @@ List<Long> invalidIds(Range r, Checker checker) {
     return val;
 }
 
-interface Checker {
-    boolean check(long id);
+interface Validator {
+    boolean isValid(long id);
 
     // extract id to digit list
-    default List<Integer> makeDigitList(long id) {
+    default List<Integer> toDigitList(long id) {
         var val = new ArrayList<Integer>();
 
         while (id > 0) {
@@ -51,11 +69,11 @@ interface Checker {
     }
 }
 
-static class DefaultChecker implements Checker {
+static class DefaultValidator implements Validator {
     @Override
-    public boolean check(long id) {
+    public boolean isValid(long id) {
 
-        var buf = makeDigitList(id);
+        var buf = toDigitList(id);
 
         // ids with an odd number of digits are valid
         if (buf.size() % 2 == 1) {
@@ -101,7 +119,7 @@ List<Range> parse(String file) {
 void partA(List<Range> ranges) {
     var badIds = new ArrayList<Long>();
     for (Range r : ranges) {
-        badIds.addAll(invalidIds(r, new DefaultChecker()));
+        badIds.addAll(invalidIds(r, new DefaultValidator()));
     }
 
     // too low : 9283763888 :/
@@ -114,7 +132,46 @@ void partA(List<Range> ranges) {
 void partB(List<Range> ranges) {
     var badIds = new ArrayList<Long>();
     for (Range r : ranges) {
-        badIds.addAll(invalidIds(r, new DefaultChecker()));
+        badIds.addAll(invalidIds(r, new RepeatValidator()));
     }
     println(badIds.stream().reduce(0L, Long::sum));
+}
+
+static class RepeatValidator implements Validator {
+    @Override
+    public boolean isValid(long id) {
+        var s = Long.toString(id);
+        int max = s.length() / 2;
+
+        for (int i = 0; i < max; i++) {
+            var chunks = split(s, i);
+            var first = chunks.getFirst();
+            boolean ok = chunks.stream().allMatch(c -> c.equals(first));
+            if (ok) {
+                return false;
+            }
+        }
+
+        // id is ok
+        // couldn't find a size where all chunks are the same
+        return true;
+    }
+
+    // Returns list of strings of length n by splitting s
+    List<String> split(String s, int n) {
+        var val = new ArrayList<String>();
+
+        int from = 0;
+        while (true) {
+            int to = from + n;
+            if (to >= s.length()) {
+                val.add(s.substring(from, s.length() - 1));
+                break;
+            }
+            val.add(s.substring(from, to));
+            from += n;
+        }
+
+        return val;
+    }
 }
