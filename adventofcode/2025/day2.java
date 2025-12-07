26c9bccb115610cba09ad4e058f082077e385758
Christian Leskowsky <christian@leskowsky.net>
2025-12-07 08:31:27 -0500

    Refactored isValid method

--- adventofcode/2025/day2.java
+++ adventofcode/2025/day2.java
@@ -1,14 +1,17 @@
 import static java.lang.IO.println;
 
 void main() throws IOException {
-    assert isValid(1188511880);
-    assert !isValid(1188511885);
+
+    var checker = new DefaultChecker();
+
+    assert checker.check(1188511880);
+    assert !checker.check(1188511885);
 
     var t1 = invalidIds(new Range(1188511880, 1188511890));
     assert 1 == t1.size();
     assert 1188511885 == t1.getFirst();
 
-    assert isValid(101);
+    assert checker.check(101);
 
     List<Range> sampleRanges = parse("inputs/day2_sample.txt");
     List<Range> ranges = parse("inputs/day2.txt");
@@ -20,39 +23,59 @@ void main() throws IOException {
 List<Long> invalidIds(Range r) {
     var val = new ArrayList<Long>();
     for (long i = r.min(); i <= r.max(); i++) {
-        if (!isValid(i)) {
-            val.add(i);
+
+        // skip good ids
+        var checker = new DefaultChecker();
+        if (checker.check(i)) {
+            continue;
         }
+
+        // add bad id
+        val.add(i);
     }
     return val;
 }
 
-boolean isValid(long id) {
+interface Checker {
+    boolean check(long id);
 
-    // extract digits to array
-    byte[] buf = new byte[50];
-    int size = 0;
-    while (id > 0) {
-        buf[size] = (byte) (id % 10);
-        id = id / 10;
-        size++;
+    // extract id to digit list
+    default List<Integer> makeDigitList(long id) {
+        var val = new ArrayList<Integer>();
+
+        while (id > 0) {
+            val.add((int) (id % 10));
+            id = id / 10;
+        }
+
+        return val;
     }
+}
 
-    // ids with an odd number of digits are valid
-    if (size % 2 == 1) {
-        return true;
-    }
+static class DefaultChecker implements Checker {
+    @Override
+    public boolean check(long id) {
 
-    // look at pairs starting at 0, mid
-    // an id is valid when we find a pair of different numbers
-    for (int i = 0, j = size / 2; j < size; i++, j++) {
-        if (buf[i] != buf[j]) {
+        var buf = makeDigitList(id);
+
+        // ids with an odd number of digits are valid
+        if (buf.size() % 2 == 1) {
             return true;
         }
-    }
 
-    // we're not valid so we must be invalid
-    return false;
+        // look at pairs starting at 0, mid
+        // an id is valid when we find a pair of different numbers
+        for (int i = 0, j = buf.size() / 2; j < buf.size(); i++, j++) {
+            int left = buf.get(i);
+            int right = buf.get(j);
+            if (left != right) {
+                return true;
+            }
+        }
+
+        // we're not valid so we must be invalid
+        return false;
+    }
 }
 
 record Range(long min, long max) {
