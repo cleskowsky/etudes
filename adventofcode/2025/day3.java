61394c469a160d9a85f5b737d0e3984cbe7b9fb1
Christian Leskowsky <christian@leskowsky.net>
2025-12-12 08:27:30 -0500

    Refactor

--- adventofcode/2025/day3.java
+++ adventofcode/2025/day3.java
@@ -1,76 +1,52 @@
-import static java.lang.IO.println;
-
 void main() throws IOException {
 
     var sampleData = Files.readAllLines(Path.of("inputs/day3_sample.txt"));
     var data = Files.readAllLines(Path.of("inputs/day3.txt"));
 
-    assert 98 == jolts("987654321111111");
-
-    // find joltages in power supply
     assert 357 == partA(sampleData);
     assert 17034 == partA(data);
 
-    partB(sampleData);
-    partB(data);
+    assert 3121910778619L == partB(sampleData);
+    assert 168798209663590L == partB(data);
 }
 
 long partA(List<String> supply) {
-    // for every bank
-    long sum = 0;
-    for (var line : supply) {
-        // find 2 batteries
-        sum += jolts(line);
-    }
 
+    long sum = 0;
+    for (var bank : supply) {
+        // find 2 batteries
+        sum += jolts(bank, 2);
+    }
     return sum;
 }
 
-// Returns max joltage in battery bank
-long jolts(String bank) {
-    long max = 0;
-
-    // first battery
-    for (int i = 0; i < bank.length() - 1; i++) {
-        var left = Character.digit(bank.charAt(i), 10) * 10;
-
-        // second battery
-        for (int j = i + 1; j < bank.length(); j++) {
-            var right = Character.digit(bank.charAt(j), 10);
-            if (left + right > max) {
-                max = left + right;
-            }
-        }
-    }
-
-    return max;
-}
-
-void partB(List<String> supply) {
+long partB(List<String> supply) {
 
     // i'm always looking for the biggest battery to the right of the
     // last one i picked reserving enough batteries in the tail to be
     // able to pick n - 1 more
 
     long sum = 0;
-
     for (var bank : supply) {
+        // find 12 batteries
+        sum += jolts(bank, 12);
+    }
+    return sum;
+}
 
-        int from = 0;
-        int remaining = 12;
-        String jolts = "";
+// Return jolts for n batteries picked from bank
+long jolts(String bank, int n) {
 
-        for (int i = remaining - 1; i >= 0; i--) {
-            var b = maxJoltsInRange(bank, from, bank.length() - i);
-            jolts += b.jolts();
-            from = b.idx() + 1;
-        }
+    int from = 0;
+    String jolts = "";
 
-        println(jolts);
-        sum += Long.parseLong(jolts);
+    for (int i = n - 1; i >= 0; i--) {
+        var b = maxJoltsInRange(bank, from, bank.length() - i);
+        jolts += b.jolts();
+        from = b.idx() + 1;
     }
 
-    println(sum);
+    return Long.parseLong(jolts);
 }
 
 record Battery(int jolts, int idx) {
