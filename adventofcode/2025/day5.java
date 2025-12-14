2ee573b8f53373351f08db6ac7e49c407cf5b84e
Christian Leskowsky <christian@leskowsky.net>
2025-12-14 14:57:14 -0500

    part b

--- adventofcode/2025/day5.java
+++ adventofcode/2025/day5.java
@@ -15,12 +15,21 @@ void main() throws IOException {
 
     // part a
 
-    var freshIdRanges = rangify(sample);
-    var ingredientIds = ingredients(sample);
-    assert 3 == partA(ingredientIds, freshIdRanges);
+    var freshIds = rangify(sample);
+    var ingredients = ingredients(sample);
+    assert 3 == partA(ingredients, freshIds);
 
     String input = Files.readString(Path.of("inputs/day5.txt"));
     assert 643 == partA(ingredients(input), rangify(input));
+
+    // part b wants me to enumerate all available ingredient ids
+    // i can probably merge overlapping ranges and then sum the differences
+    // of the ranges left ...
+    //
+    // does sorting by r.begin() help?
+
+    System.out.println(partB(rangify(sample)));
+    System.out.println(partB(rangify(input)));
 }
 
 record Range(long begin, long end) {
@@ -72,4 +81,62 @@ int partA(List<Long> ingredientIds, List<Range> freshIdRanges) {
     }
 
     return cnt;
+}
+
+long partB(List<Range> freshIds) {
+
+    System.out.println("partB");
+
+    freshIds.sort(Comparator.comparingLong(Range::begin));
+    System.out.println(freshIds);
+
+    int size = freshIds.size();
+    freshIds = merge(freshIds);
+    while (freshIds.size() != size) {
+        size = freshIds.size();
+        freshIds = merge(freshIds);
+    }
+
+    System.out.println(freshIds);
+
+    return freshIds.stream()
+            .map(r -> r.end() - r.begin() + 1)
+            .reduce(0L, Long::sum);
+}
+
+List<Range> merge(List<Range> freshIds) {
+    var val = new ArrayList<Range>();
+
+    for (int i = 0; i < freshIds.size(); i++) {
+        var left = freshIds.get(i);
+        if (i + 1 >= freshIds.size()) {
+            val.add(left);
+            continue;
+        }
+        var right = freshIds.get(i + 1);
+        if (left.end() >= right.begin()) {
+            // Left and right are mergeable
+            // Since we've made a new range, we
+            // won't add the original ranges to
+            // our new merge list
+            var merged = new Range(
+                    left.begin(),
+                    Math.max(left.end(), right.end())
+            );
+            val.add(merged);
+            System.out.printf("merged left_begin=%d left_end=%d right_begin=%d right_end=%d merged_begin=%d merged_end=%d\n",
+                    left.begin(), left.end(),
+                    right.begin(), right.end(),
+                    merged.begin(), merged.end());
+            i++;
+        } else {
+            // Can't merge the 2 ranges
+            // I know left range is not mergeable
+            // but I don't know about right yet (it
+            // could merge with its forward sibling)
+            val.add(left);
+        }
+    }
+
+    return val;
 }
